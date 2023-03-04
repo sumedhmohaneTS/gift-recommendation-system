55e0abdea27cc80e8ae27841eb969332a1377b7f
Sumedh Mohane <sumedhmohane@gmail.com>
2023-03-05 02:33:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ module.exports = (container) => {
     const app = express();
     const port = process.env.PORT || 3000;
 
+    app.use(cors());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
     app.use((req, res, next) => {
